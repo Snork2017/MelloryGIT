4d0d01c0255f5c76693a1b7cebb9af1c1870812b
Kantemir <gukezhebv28@gmail.com>
2020-12-08 12:50:11 +0200

    feature_websockets_cabinetChat

--- testMain.go
+++ testMain.go
@@ -1,14 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 
 	"./cache"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+var upgrader = websocket.Upgrader{}
 var cacheUser *cache.CacheUser
+
+type Message struct {
+	User    string `json:"user"`
+	Message string `json:"message"`
+}
+
+func main() {
+	cacheUser = cache.NewUser()
+	newUser := cache.User{
+		Login:    "Mellory",
+		Password: "123",
+	}
+	cacheUser.SetUser("Mellory", newUser)
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "testMain.html", nil)
+	})
+	r.POST("/login", Login)
+	r.GET("/ws", handleConnections)
+	go handleMessages()
+	log.Println("http server started on :8000")
+	err := r.Run()
+	if err != nil {
+		panic(err.Error())
+	}
+}
 
 func Login(c *gin.Context) {
 	person := cache.User{
@@ -27,28 +58,50 @@ func Login(c *gin.Context) {
 				cacheUser.RUnlock()
 				return
 			} else {
-				c.JSON(200, fmt.Sprintf("Добро пожаловать %v", person.Login))
+				c.HTML(http.StatusOK, "cabinet.html", nil)
 			}
 		}
 	}
 	cacheUser.RUnlock()
 }
 
-func main() {
-	cacheUser = cache.NewUser()
-	newUser := cache.User{
-		Login:    "Mellory",
-		Password: "123",
-	}
-	cacheUser.SetUser("Mellory", newUser)
-	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "testMain.html", nil)
-	})
-	r.POST("/login", Login)
-	err := r.Run()
+func handleConnections(c *gin.Context) {
+	// Upgrade initial GET request to a websocket
+	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
-		fmt.Println("Ошибка при запуске сервера", err.Error())
+		log.Fatal(err)
+	}
+	// Make sure we close the connection when the function returns
+	defer ws.Close()
+
+	clients[ws] = true
+
+	for {
+		var msg Message
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		// Send the newly received message to the broadcast channel
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// Grab the next message from the broadcast channel
+		msg := <-broadcast
+		// Send it out to every client that is currently connected
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
 	}
 }
